8284f74bb3d10affdcb8ab5edc8d6fd13cf4b1ad
Azad Erdoğan <azaderdogan@outlook.com.tr>
2021-01-25 19:10:06 +0300

    Add project files.

---
+++ KelimeOyunu/GameForm.Designer.cs
@@ -0,0 +1,345 @@
+﻿
+namespace KelimeOyunu
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.question = new System.Windows.Forms.Label();
+            this.letterTakeBtn = new System.Windows.Forms.Button();
+            this.otherAsk = new System.Windows.Forms.Button();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.letterNote = new System.Windows.Forms.Label();
+            this.lasdlksajlkşd = new System.Windows.Forms.Label();
+            this.point = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.answerTxt = new System.Windows.Forms.TextBox();
+            this.panel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(37, 134);
+            this.label1.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Soru:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(37, 207);
+            this.label2.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kelime:";
+            // 
+            // question
+            // 
+            this.question.AutoSize = true;
+            this.question.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.question.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.question.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.question.Location = new System.Drawing.Point(165, 134);
+            this.question.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.question.Name = "question";
+            this.question.Size = new System.Drawing.Size(120, 26);
+            this.question.TabIndex = 5;
+            this.question.Text = "Soru alanı";
+            // 
+            // letterTakeBtn
+            // 
+            this.letterTakeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.letterTakeBtn.ForeColor = System.Drawing.Color.White;
+            this.letterTakeBtn.Location = new System.Drawing.Point(43, 329);
+            this.letterTakeBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.letterTakeBtn.Name = "letterTakeBtn";
+            this.letterTakeBtn.Size = new System.Drawing.Size(172, 53);
+            this.letterTakeBtn.TabIndex = 7;
+            this.letterTakeBtn.Text = "Harf Al";
+            this.letterTakeBtn.UseVisualStyleBackColor = false;
+            this.letterTakeBtn.Click += new System.EventHandler(this.letterTakeBtn_Click);
+            // 
+            // otherAsk
+            // 
+            this.otherAsk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.otherAsk.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.otherAsk.Location = new System.Drawing.Point(539, 409);
+            this.otherAsk.Margin = new System.Windows.Forms.Padding(4);
+            this.otherAsk.Name = "otherAsk";
+            this.otherAsk.Size = new System.Drawing.Size(284, 53);
+            this.otherAsk.TabIndex = 8;
+            this.otherAsk.Text = "Diğer Soru";
+            this.otherAsk.UseVisualStyleBackColor = false;
+            this.otherAsk.Click += new System.EventHandler(this.otherAsk_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::KelimeOyunu.Properties.Resources.d;
+            this.pictureBox7.Location = new System.Drawing.Point(490, 42);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(88, 54);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 16;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::KelimeOyunu.Properties.Resources._40199191;
+            this.pictureBox6.Location = new System.Drawing.Point(602, 42);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(88, 54);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 15;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::KelimeOyunu.Properties.Resources.r;
+            this.pictureBox5.Location = new System.Drawing.Point(382, 42);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(88, 54);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 14;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::KelimeOyunu.Properties.Resources.o;
+            this.pictureBox4.Location = new System.Drawing.Point(276, 42);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(88, 54);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::KelimeOyunu.Properties.Resources.w1;
+            this.pictureBox3.Location = new System.Drawing.Point(162, 42);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(88, 54);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::KelimeOyunu.Properties.Resources.scrabble;
+            this.pictureBox2.Location = new System.Drawing.Point(710, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(131, 68);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KelimeOyunu.Properties.Resources.star__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 68);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // letterNote
+            // 
+            this.letterNote.AutoSize = true;
+            this.letterNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.letterNote.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.letterNote.Location = new System.Drawing.Point(49, 282);
+            this.letterNote.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.letterNote.Name = "letterNote";
+            this.letterNote.Size = new System.Drawing.Size(311, 31);
+            this.letterNote.TabIndex = 17;
+            this.letterNote.Text = "Not: Kelime 10 harflidir";
+            // 
+            // lasdlksajlkşd
+            // 
+            this.lasdlksajlkşd.AutoSize = true;
+            this.lasdlksajlkşd.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lasdlksajlkşd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lasdlksajlkşd.Location = new System.Drawing.Point(586, 282);
+            this.lasdlksajlkşd.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.lasdlksajlkşd.Name = "lasdlksajlkşd";
+            this.lasdlksajlkşd.Size = new System.Drawing.Size(81, 31);
+            this.lasdlksajlkşd.TabIndex = 18;
+            this.lasdlksajlkşd.Text = "Puan";
+            // 
+            // point
+            // 
+            this.point.AutoSize = true;
+            this.point.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.point.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.point.Location = new System.Drawing.Point(515, 282);
+            this.point.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(30, 31);
+            this.point.TabIndex = 19;
+            this.point.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.timerLabel.Location = new System.Drawing.Point(41, 483);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(0, 46);
+            this.timerLabel.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(43, 409);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 53);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Tahmin Et";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // answerTxt
+            // 
+            this.answerTxt.Location = new System.Drawing.Point(236, 329);
+            this.answerTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.answerTxt.Name = "answerTxt";
+            this.answerTxt.Size = new System.Drawing.Size(431, 53);
+            this.answerTxt.TabIndex = 9;
+            this.answerTxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel
+            // 
+            this.panel.AutoScroll = true;
+            this.panel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel.ForeColor = System.Drawing.Color.Red;
+            this.panel.Location = new System.Drawing.Point(162, 196);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(505, 51);
+            this.panel.TabIndex = 22;
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(24F, 46F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.lasdlksajlkşd);
+            this.Controls.Add(this.letterNote);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.answerTxt);
+            this.Controls.Add(this.otherAsk);
+            this.Controls.Add(this.letterTakeBtn);
+            this.Controls.Add(this.question);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.Name = "GameForm";
+            this.Text = "AddQuestions";
+            this.Load += new System.EventHandler(this.GameForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label question;
+        private System.Windows.Forms.Button letterTakeBtn;
+        private System.Windows.Forms.Button otherAsk;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label letterNote;
+        private System.Windows.Forms.Label lasdlksajlkşd;
+        private System.Windows.Forms.Label point;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox answerTxt;
+        private System.Windows.Forms.Panel panel;
+    }
+}
---
+++ KelimeOyunu/GameForm.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace KelimeOyunu
+{
+    public partial class GameForm : Form
+    {
+        private int totalPoint = 0;
+        private List<AskQuestion> askQuestions;
+        Random random = new Random();
+        private int randomNumber;
+        private int randomLetterNumber;
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void otherAsk_Click(object sender, EventArgs e)
+        {
+            randomNumber = random.Next(0, (askQuestions.Count - 1));
+           
+            panel.Controls.Clear();
+            letters.Clear();
+            openedIndex.Clear();
+            openedLetterLabel.Clear();
+            
+            setAsk();
+        }
+
+        async void getAll()
+        {
+            askQuestions = DbHelper.GetDbHelper().GetAllQuestion();
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+           getAll();
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+            randomNumber = random.Next(0,(askQuestions.Count-1 ));
+            setAsk();
+        }
+
+       
+        
+        private int minute = 4;
+        private int second = 59;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            second--;
+            if (second == 0)
+            {
+                minute--;
+                second = 59;
+            }
+
+            if (minute == 0 && second == 0)
+            {
+                finishGame();
+            }
+            timerLabel.Text = minute+" : "+second;
+        }
+
+        private void finishGame()
+        {
+
+        }
+
+        private HashSet<Label> openedLetterLabel = new HashSet<Label>();
+        private List<int> openedIndex = new List<int>();
+        private void letterTakeBtn_Click(object sender, EventArgs e)
+        {
+            
+            int nowLetter = 0;
+            if (openedIndex.Count == currentaskQuestion.word.Length)
+            {
+                return;
+            }
+            while (true)
+            {
+                bool isSelected = false;
+                nowLetter = random.Next(0, currentaskQuestion.word.Length);
+                foreach (int i in openedIndex)
+                {
+                    if (nowLetter == i)
+                    {
+                        Console.WriteLine("Seçilen index seçildi");
+                        isSelected = true;
+                        
+
+                    }
+
+                }
+
+                if (!isSelected)
+                {
+                 break;  
+                 return;
+                }
+            }
+
+            Label label;
+            
+            label = letters.ElementAt(nowLetter);
+            
+            totalPoint -= 10;
+            
+            point.Text = totalPoint.ToString();
+            
+            openedIndex.Add(nowLetter);
+            
+            label.Visible = true;
+            openedLetterLabel.Add(label);
+            Console.WriteLine("Harf alındı.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //tahmin et
+            if (answerTxt.Text.Equals(currentaskQuestion.word))
+            {
+                //todo
+                totalPoint = (currentaskQuestion.word.Length - openedIndex.Count) * 10;
+                point.Text = totalPoint.ToString();
+            }
+        }
+
+        private AskQuestion currentaskQuestion = null;
+        private List<Label> letters = new List<Label>();
+        private void setAsk()
+        {
+            
+            currentaskQuestion = askQuestions.ElementAt(randomNumber);
+            question.Text = currentaskQuestion.ask;
+            randomLetterNumber = currentaskQuestion.word.Length;
+            letterNote.Text = "Not: Kelime " + randomLetterNumber + " harflidir";
+            for (int i = 0; i < currentaskQuestion.word.Length; i++)
+            {
+                Label label = new Label();
+                label.Font = new Font("Arial", 25, FontStyle.Bold);
+                label.AutoSize = true;
+                label.Location = new Point(i * 40, 0);
+                label.Visible = false;
+                if (i  == currentaskQuestion.word.Length)
+                {
+                    label.Text = currentaskQuestion.word.Substring(i+1);
+                }
+                else
+                {
+                    label.Text = currentaskQuestion.word.Substring(i, 1);
+                    Console.WriteLine(label.Text);
+                }
+                letters.Add(label);
+
+                panel.Controls.Add(label);
+
+                //listBox1.Items.Add(label);
+                Console.WriteLine("label eklendi");
+                
+            }
+            
+            
+        }
+    }
+}
